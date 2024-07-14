35bf005687f8ac8ccbe3485121eddb0b89328d2b
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-07-15 00:50:20 +0530

    functions in js

---
+++ functions(method-1)/0307/addition1.js
@@ -0,0 +1,9 @@
+function addition(){
+    let a=10;
+    let b=20;
+    let s= a+b;
+    console.log("first number=",a);
+    console.log("second number=",b);
+    console.log("sum =",s);
+}
+addition();
---
+++ functions(method-1)/0307/last-dig.js
@@ -0,0 +1,8 @@
+function last_digit(){
+    let n=123;
+    let a=n%10;
+    console.log("number is ",n);
+    console.log("last digit is",a);
+
+}
+last_digit();
