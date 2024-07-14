fc05148ac9e44945f3d18d5d2ec3d0734f822528
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-07-14 23:21:49 +0530

    do-while loop in js

---
+++ do-while loop/1906/even.js
@@ -0,0 +1,6 @@
+console.log("even numbers between 1 to 10 are");
+let i=2;
+do{
+    console.log(i);
+    i=i+2;
+}while(i<=10);
---
+++ do-while loop/1906/odd.js
@@ -0,0 +1,8 @@
+console.log("odd numbers between 1 and 10 are");
+let i=1;
+do{
+    if(i%2!=0){
+        console.log(i);
+    }
+    i++;
+}while(i<=10);
