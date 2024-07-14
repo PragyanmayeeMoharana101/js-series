39778154f5bf621b4f84acf7364207b3babb9560
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-07-15 00:59:44 +0530

    functions in js(method-2)

---
+++ functions(method-2)/0307/addition2.js
@@ -0,0 +1,10 @@
+function addition(){
+    let a=10;
+    let b=20;
+    let s= a+b;
+    console.log("first number=",a);
+    console.log("second number=",b);
+    return s;
+}
+let r=addition();
+console.log("sum =",r);
