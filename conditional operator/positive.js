2a5a3477509c85df5e5076c65aaf2925650b1643
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-07-31 02:21:48 +0530

    deleted

--- conditional operator/positive.js
+++
@@ -1,3 +0,0 @@
-let n= 5;
-let ans = n>=0? "is positive" : "is negetive";
-console.log(n ,ans);
--- conditional operator/vote.js
+++
@@ -1,4 +0,0 @@
- let age = 14;
- let ans = age>=18 ? "person is eligible to vote":"person is not eligible to vote";
- console.log("age is",age);
- console.log(ans);
