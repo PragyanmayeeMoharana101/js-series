638972b788e41e090e00085714a66b6be51ce124
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-08-15 02:08:27 +0530

    strings in js

---
+++ string/1308/reverse.js
@@ -0,0 +1,7 @@
+let studentname= "pragyan";
+let rname = "";
+let a= studentname.length;
+for(let i = a-1; i>=0; i--){
+    rname= rname+ studentname[i];
+}
+console.log(rname);
---
+++ string/1308/string.js
@@ -0,0 +1,5 @@
+let studentname = "ram";
+console.log(studentname);
+for(let i =0;i<studentname.length;i++){
+    console.log(studentname[i]);
+}
