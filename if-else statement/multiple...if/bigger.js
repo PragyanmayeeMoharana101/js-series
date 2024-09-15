0de99c8f673febdbc57e8de5ec5ee212170b8c37
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-09-16 01:12:48 +0530

    multiple if in js

---
+++ if-else statement/multiple...if/bigger.js
@@ -0,0 +1,11 @@
+let a = 2;
+let b = 7;
+console.log("first number = ",a);
+console.log("second number = ",b);
+if(a<b){
+    console.log("bigger number = ",b);
+}else if(b<a){
+    console.log("bigger number = ",a);
+}else{
+    console.log("both numbers are same");
+}
