e5c575fe1c1702059980eff8b3f5cfbeaaa68514
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-09-18 01:35:09 +0530

    multiple if in js

---
+++ if-else statement/multiple...if/week.js
@@ -0,0 +1,19 @@
+let n =5;
+console.log("number = ",n);
+if(n==1){
+    console.log("monday");
+}else if(n==2){
+    console.log("tuesday");
+}else if(n==3){
+    console.log("wednesday");
+}else if(n==4){
+    console.log("thrusday");
+}else if(n==5){
+    console.log("friday");
+}else if(n==6){
+    console.log("saturday");
+}else if(n==7){
+    console.log("sunday");
+}else{
+    console.log("invalid digit");
+}
