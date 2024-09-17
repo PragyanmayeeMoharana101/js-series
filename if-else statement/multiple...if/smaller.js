c6e58bf1b1578194bbc4da6dc1daf970661a6c75
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-09-18 01:34:35 +0530

    multiple if in js

---
+++ if-else statement/multiple...if/smaller.js
@@ -0,0 +1,13 @@
+let a = 2;
+let b = 7;
+let c = 8;
+console.log("first number = ",a);
+console.log("second number = ",b);
+console.log("thrid number = ",c);
+if(a<b && a<c){
+    console.log("smaller number = ",a);
+}else if(b<a && b<c){
+    console.log("smaller number = ",b);
+}else{
+    console.log("smaller number = ",c);
+}
