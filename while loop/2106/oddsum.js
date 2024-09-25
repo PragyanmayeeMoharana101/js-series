0887b0778800c8be5eea6a8e3008205f8bacf695
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-09-26 03:05:51 +0530

    for while in js

---
+++ while loop/2106/oddsum.js
@@ -0,0 +1,12 @@
+console.log("odd numbers between 1 to 10 are");
+let s=0;
+let i=1;
+while(i<=10){
+    if(i%2!=0){
+        console.log(i);
+        s=s+i;
+    }
+    i++;
+}
+
+console.log("sum of odd numbers are",s);
