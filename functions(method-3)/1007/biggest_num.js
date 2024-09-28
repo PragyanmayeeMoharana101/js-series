77c79e3f74a126f189f6e7fde032ce2691066af9
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-09-29 00:09:51 +0530

    function in js

---
+++ functions(method-3)/1007/biggest_num.js
@@ -0,0 +1,12 @@
+function biggestnum(a,b){
+    if(a>b){
+        console.log("biggest number is ",a);
+    }else{
+        console.log("biggest number is ",b);
+    }
+}
+let a = 5;
+let b = 3;
+console.log("first number =",a);
+console.log("second number =",b);
+biggestnum(a,b);
