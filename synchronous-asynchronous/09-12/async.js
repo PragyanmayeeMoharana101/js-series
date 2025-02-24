c4071e38f68f07a7e5bde2fc93440aa1038179bc
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2025-02-25 02:28:59 +0530

    synchronous and asynchronous function in js

---
+++ synchronous-asynchronous/09-12/async.js
@@ -0,0 +1,32 @@
+function register(){
+    setTimeout(()=>{
+        console.log("register end");
+    },1000)
+}
+function sendmail(){
+    setTimeout(()=>{
+        console.log("send mail end");
+    },1000)
+}
+function login(){
+    setTimeout(()=>{
+        console.log("login end");
+    },1000)
+}
+function getdata(){
+    setTimeout(()=>{
+        console.log("get data end");
+    },1000)
+}
+function displaydata(){
+    setTimeout(()=>{
+        console.log("display data end");
+    },1000)
+}
+register();
+sendmail();
+login();
+getdata();
+displaydata();
+console.log("other application executed")
+      
