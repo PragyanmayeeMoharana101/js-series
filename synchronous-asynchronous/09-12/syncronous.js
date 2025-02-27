7fc25107c6abdd9e0c520f456f43df73f3a12744
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2025-02-28 04:48:38 +0530

    synchronous and asynchronous function in js

---
+++ synchronous-asynchronous/09-12/syncronous.js
@@ -0,0 +1,22 @@
+function register(){
+ console.log("register end");
+}
+function sendmail(){
+    console.log("send mail end");
+}
+function login(){
+    console.log("login end");
+}
+function getdata(){
+    console.log("get data end");
+}
+function displaydata(){
+    console.log("display data end");
+}
+register();
+sendmail();
+login();
+getdata();
+displaydata();
+
+   
